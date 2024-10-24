834e73cd8cf5b302e7fbf81ede616431e443a3ef
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-10-24 19:14:00 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Contador from './components/Contador';
+
+function App() {
+  return (
+    <div className='App'>
+      <Contador/>
+    </div>
+  );
+}
+
+export default App
